94ed010739ad56573d0e57fb2daaad5d994ad9d9
gbern13 <159074543+gbern13@users.noreply.github.com>
2025-12-29 23:32:53 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,14 +2,21 @@ import statsmodels.api as sm
 import pandas as pd
 
 def wls_model(df, target, weight_col, drop_cols=None, cov_type='HC1'):
-    
+    """
+    df : pandas DataFrame
+    target : string
+    weight_col : string
+    drop_cols : list of strings
+    cov_type : string
+    """
     if drop_cols is None:
         drop_cols = []
     
     to_drop = [target, weight_col] + drop_cols
     
-    if 'CNTSTUID'or 'id' in df.columns:
+    if 'CNTSTUID' in df.columns:# on enlève CNTSTUID et id si ils existent
         to_drop.append('CNTSTUID')
+    if 'id' in df.columns:
         to_drop.append('id')
         
     to_drop = list(set([c for c in to_drop if c in df.columns]))
@@ -24,14 +31,20 @@ def wls_model(df, target, weight_col, drop_cols=None, cov_type='HC1'):
     return model
 
 def ols_model(df, target,drop_cols=None, cov_type='HC1'):
-    
+    """
+    df : pandas DataFrame
+    target : string
+    drop_cols : list of strings
+    cov_type : string
+    """
     if drop_cols is None:
         drop_cols = []
     
     to_drop = [target] + drop_cols
     
-    if 'CNTSTUID'or 'id' in df.columns:
+    if 'CNTSTUID' in df.columns:# on enlève CNTSTUID et id si ils existent
         to_drop.append('CNTSTUID')
+    if 'id' in df.columns:
         to_drop.append('id')
         
     to_drop = list(set([c for c in to_drop if c in df.columns]))
